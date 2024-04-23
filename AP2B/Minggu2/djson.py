e8c2e87cea06c392f6fac626e58be6d38b3cf9e8
Wardiansyahf <ardi.dl738@gmail.com>
2024-04-23 15:22:56 +0700

    update M2

--- AP2B/Minggu2/djson.py
+++ AP2B/Minggu2/djson.py
@@ -3,4 +3,4 @@ import json
 with open("tes.json") as file_json:
     data = json.loads(file_json.read())
     
-print(json.dumps(data, indent=4, sort_keys=True))
+print(json.dumps(data, indent=1, sort_keys=False))
--- AP2B/Minggu2/request.py
+++ AP2B/Minggu2/request.py
@@ -8,22 +8,26 @@ payload = {"nama": "Wardiansyah",
            }
 # digunakan pada request get agar kita bisa mencari postingan
 parameter = {'id': 2}
+
+# endpoint yang dituju
+endpoint = 'https://jsonplaceholder.typicode.com/posts'
+
 # post untuk mengirimkan data ke API jsonplaceholder
-addpost = requests.post(
-    'https://jsonplaceholder.typicode.com/posts', data=payload)
+addpost = requests.post(endpoint, data=payload)
 # untuk mengambil postingan yang ada di jsonplaceholder sesuai parameter id
-getPostbyId = requests.get(
-    'https://jsonplaceholder.typicode.com/posts', params=parameter)
+getPostbyId = requests.get(endpoint, params=parameter)
 
-
+print("[POST")
 if (addpost.status_code == 201):
    # untuk mengecek apakah data berhasil terkirim atau tidak
     print('Data berhasil dikirim')
     print(addpost.text)
+    print("\nStatus code : ",addpost.status_code)
 else:
     # jika gagal maka akan menampilkan data gagal dikirim
     print('Data gagal dikirim')
 
+print("\n[GET]")
 # menampilkan data yang sudah diambil
 print(getPostbyId.text)
-print(getPostbyId.status_code)
+print("\nStatus code : ",getPostbyId.status_code)
